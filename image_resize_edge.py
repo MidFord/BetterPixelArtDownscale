7b9dd522510e29d25209a13ed020b38a4c6df75a
MidFord <87622554+MidFord@users.noreply.github.com>
2023-12-12 21:13:59 -0700

    Update image_resize_edge.py
    Reorganised headings and added a description to the functions to make it more intuitive, and parameters were added for better customisation.

--- image_resize_edge.py
+++ image_resize_edge.py
@@ -1,8 +1,11 @@
 import image_edges as imeg
 import image_resize as imre
 
-def processImage(image_path, factor_x, factor_y):
-    edges_image = imeg.processImage(image_path,factor_x,factor_y)
+def processImage(image_path, factor_x, factor_y,iclude_outline=True, include_edges=True, only_edges=False, outline_threshold=1.32,center_threshold=0.0,sharp_radius=1,sharp_percent=1, color_simplifier=2, color_diff_threshold=10, color_light_threshold=10):
+    """
+    Processes an image by finding and resizing its edges
+    """
+    edges_image = imeg.processImage(image_path,factor_x,factor_y, iclude_outline, include_edges, only_edges, outline_threshold,center_threshold,sharp_radius,sharp_percent, color_simplifier, color_diff_threshold, color_light_threshold)
     image_downscaled = imre.processImage(image_path,factor_x,factor_y)
     image_merged = imeg.CombineImageInCaps(edges_image,image_downscaled)
     return image_merged
