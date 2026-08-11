8d05a734430fbe6d7c7a0042491b71789d61d77d
MidFord327 <87622554+MidFord@users.noreply.github.com>
2026-08-11 09:13:35 -0700

    Export JavaScript API

---
+++ javascript/src/index.js
@@ -0,0 +1,10 @@
+export { DownscaleOptions } from './options.js';
+export {
+  downscale,
+  downscaleByFactor,
+  downscaleByFactor as downscale_by_factor,
+  edgeLayer,
+  edgeLayer as edge_layer,
+  fromImageData,
+  toImageData,
+} from './core.js';
