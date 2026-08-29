fb0714334880d108f93ccb267e2c381b19a49c64
MidFord327 <87622554+MidFord@users.noreply.github.com>
2026-08-28 22:13:50 -0700

    Export semantic Python API

--- python/better_pixel_art_downscale/__init__.py
+++ python/better_pixel_art_downscale/__init__.py
@@ -1,13 +1,29 @@
-"""Edge-aware, silhouette-stable pixel-art downscaling."""
+"""Edge-aware and semantic pixel-art downscaling."""
 
 from .core import downscale, downscale_by_factor, downscale_file, edge_layer
 from .options import DownscaleOptions
+from .semantic import (
+    ContentHint,
+    SemanticAnalysis,
+    SemanticMode,
+    SemanticOptions,
+    analyze,
+    downscale_semantic,
+    downscale_semantic_by_factor,
+)
 
 __all__ = [
+    "ContentHint",
     "DownscaleOptions",
+    "SemanticAnalysis",
+    "SemanticMode",
+    "SemanticOptions",
+    "analyze",
     "downscale",
     "downscale_by_factor",
     "downscale_file",
+    "downscale_semantic",
+    "downscale_semantic_by_factor",
     "edge_layer",
 ]
 
