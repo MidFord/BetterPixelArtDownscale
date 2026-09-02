dc9beb9c2a03a2c890b1a296aa8c24f3c3d82f90
MidFord327 <87622554+MidFord@users.noreply.github.com>
2026-09-01 19:48:42 -0700

    Export semantic v3 JavaScript API

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -8,3 +8,27 @@ export {
   fromImageData,
   toImageData,
 } from './core.js';
+export {
+  ContentHint,
+  SemanticMode,
+  SemanticOptions,
+  analyze,
+  downscaleSemantic,
+  downscaleSemantic as downscale_semantic,
+  downscaleSemanticByFactor,
+  downscaleSemanticByFactor as downscale_semantic_by_factor,
+  inferContentHint,
+  spriteDownscale,
+  surfaceDownscale,
+} from './semantic.js';
+export {
+  CutoutPolicy,
+  analyzeCutout,
+  analyzeCutout as analyze_cutout,
+  downscaleCutout,
+  downscaleCutout as downscale_cutout,
+  downscaleSemanticV3,
+  downscaleSemanticV3 as downscale_semantic_v3,
+  downscaleSemanticV3ByFactor,
+  downscaleSemanticV3ByFactor as downscale_semantic_v3_by_factor,
+} from './semantic_v3.js';
